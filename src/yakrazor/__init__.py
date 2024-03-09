c868834529aa3ea23972e9a685b19721b242d34f
Harshad Sharma <harshad@sharma.io>
2024-03-09 10:31:09 +0530

    Add YakrazorAPI skeleton.

---
+++ src/yakrazor/__init__.py
@@ -0,0 +1,9 @@
+"""
+Yakrazor
+--------
+"""
+
+from yakrazor.api import YakrazorAPI
+
+
+__all__ = ["YakrazorAPI"]
---
+++ src/yakrazor/api.py
@@ -0,0 +1,5 @@
+class YakrazorAPI:
+    """
+    YakrazorAPI
+    -----------
+    """
