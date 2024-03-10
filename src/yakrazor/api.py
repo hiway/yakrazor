675c307f0ef96b1c155f99bf2e35ce8262b5b80b
Harshad Sharma <harshad@sharma.io>
2024-03-11 03:11:12 +0530

    Separate TasksAPI class, access via api.task.

--- src/yakrazor/api.py
+++ src/yakrazor/api.py
@@ -5,35 +5,19 @@ from yakrazor import database
 from yakrazor.models import Task, TaskStatus
 
 
-@dataclass
-class YakrazorAPI:
-    """
-    YakrazorAPI
-    -----------
-    """
-
-    database_url: str
-
-    async def start(self) -> None:
-        await database.open(self.database_url)
-
-    async def stop(self) -> None:
-        await database.close()
-
-    async def create_task(self, name: str, status: str) -> dict:
+class TasksAPI:
+    async def create(self, name: str, status: str) -> dict:
         task = await Task.create(
             name=name,
             status_value=TaskStatus[status.upper()],
         )
         return task
 
-    async def get_task(self, uuid: str) -> dict:
+    async def by_uuid(self, uuid: str) -> dict:
         task = await Task.get(uuid=uuid)
         return task
 
-    async def update_task(
-        self, uuid: str, name: str = None, status: str = None
-    ) -> dict:
+    async def update(self, uuid: str, name: str = None, status: str = None) -> dict:
         task = await Task.get(uuid=uuid)
         if name:
             task.name = name
@@ -43,14 +27,31 @@ class YakrazorAPI:
         await task.save()
         return task
 
-    async def delete_task(self, uuid: str) -> None:
+    async def delete(self, uuid: str) -> None:
         task = await Task.get(uuid=uuid)
         await task.delete()
 
-    async def list_tasks(self) -> list:
+    async def list(self) -> list:
         tasks = await Task.all()
         return tasks
 
-    async def filter_tasks_by_status(self, status: str) -> list:
+    async def filter_by_status(self, status: str) -> list:
         tasks = await Task.filter(status_value=TaskStatus[status.upper()])
         return tasks
+
+
+@dataclass
+class YakrazorAPI:
+    """
+    YakrazorAPI
+    -----------
+    """
+
+    database_url: str
+    task: TasksAPI = TasksAPI()
+
+    async def start(self) -> None:
+        await database.open(self.database_url)
+
+    async def stop(self) -> None:
+        await database.close()
--- tests/test_api.py
+++ tests/test_api.py
@@ -14,41 +14,41 @@ async def api():
 
 
 async def test_create_task(api):
-    task = await api.create_task(name="test", status="todo")
+    task = await api.task.create(name="test", status="todo")
     assert task.name == "test"
     assert task.status == "TODO"
 
 
 async def test_get_task(api):
-    task = await api.create_task(name="test", status="todo")
-    retrieved_task = await api.get_task(uuid=task.uuid)
+    task = await api.task.create(name="test", status="todo")
+    retrieved_task = await api.task.by_uuid(uuid=task.uuid)
     assert task == retrieved_task
 
 
 async def test_update_task(api):
-    task = await api.create_task(name="test", status="todo")
-    updated_task = await api.update_task(uuid=task.uuid, name="test2")
+    task = await api.task.create(name="test", status="todo")
+    updated_task = await api.task.update(uuid=task.uuid, name="test2")
     assert updated_task.name == "test2"
 
 
 async def test_delete_task(api):
-    task = await api.create_task(name="test", status="todo")
-    await api.delete_task(uuid=task.uuid)
+    task = await api.task.create(name="test", status="todo")
+    await api.task.delete(uuid=task.uuid)
     with pytest.raises(DoesNotExist):
-        await api.get_task(uuid=task.uuid)
+        await api.task.by_uuid(uuid=task.uuid)
 
 
 async def test_list_tasks(api):
-    task1 = await api.create_task(name="test1", status="todo")
-    task2 = await api.create_task(name="test2", status="todo")
-    tasks = await api.list_tasks()
+    task1 = await api.task.create(name="test1", status="todo")
+    task2 = await api.task.create(name="test2", status="todo")
+    tasks = await api.task.list()
     assert task1 in tasks
     assert task2 in tasks
 
 
 async def test_filter_tasks_by_status(api):
-    task1 = await api.create_task(name="test1", status="todo")
-    task2 = await api.create_task(name="test2", status="doing")
-    tasks = await api.filter_tasks_by_status(status="Doing")
+    task1 = await api.task.create(name="test1", status="todo")
+    task2 = await api.task.create(name="test2", status="doing")
+    tasks = await api.task.filter_by_status(status="Doing")
     assert task2 in tasks
     assert task1 not in tasks
